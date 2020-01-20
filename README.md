73c894b2a0e355ca68f13bfddb64bbbbcb62ec3b
kamui58 <owlprotectthewillage.58@gmail.com>
2020-01-20 21:42:02 +0900

    groupFunction_create

--- README.md
+++ README.md
@@ -41,7 +41,7 @@ Things you may want to cover:
 
 |Column|Type|Options|
 |------|----|-------|
-|name|string|null: false|
+|name|string|null: false,unique: true|
 
 ### Association
 - has_many :users_groups
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "messages#index"
-
+  root "groups#index"
+  resources :messages, only: :index
   resources :users, only: [:edit, :update]
-
-
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
