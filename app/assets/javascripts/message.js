224f2c22491b13dbf5148020d8ed53aca69523a1
kamui58 <owlprotectthewillage.58@gmail.com>
2020-01-27 19:45:00 +0900

    auto-updating_update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,37 @@
 $(function(){
 
+  var reloadMessages = function(){
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'GET',
+      data: {id: last_message_id},
+      dataTyoe: 'json'
+    })
+    .done(function(messages){
+
+      if (messages.length !== 0) {
+        console.log(messages)
+        var insertHTML = '';
+        $.each(messages, function(i, message){
+          insertHTML += buildHTML(message)
+        });
+        $('.main__chatBox').append(insertHTML);
+        $('.main__chatBox').animate({ scrollTop: $('.main__chatBox')[0].scrollHeight});
+        $("#new_message")[0].reset();
+        $(".main__footer__formBtn").prop("disabled", false);
+      }
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+
   function buildHTML(message){
     if (message.image) {
       var html =
-      ` <div class="main__chatBox__user">
+    ` <div class="message" data-message:id=${message.id}>
+        <div class="main__chatBox__user">
           <div class="main__chatBox__user__nickname">
             ${message.user_name}
           </div>
@@ -16,11 +44,13 @@ $(function(){
             ${message.body}
           </p>
         </div>
-        <img class="main__chatBox__message__image" src=${message.image}>`
+        <img class="main__chatBox__message__image" src=${message.image}>
+      </div>`
       return html;
     } else {
       var html =
-      `<div class="main__chatBox__user">
+  ` <div class="message" data-message-id=${message.id}>
+      <div class="main__chatBox__user">
         <div class="main__chatBox__user__nickname">
           ${message.user_name}
         </div>
@@ -32,7 +62,8 @@ $(function(){
         <p class="main__chatBox__message__body">
           ${message.body}
         </p>
-      </div>`
+      </div>
+    </div>`
     return html;
     };
   }
@@ -63,4 +94,7 @@ $(function(){
       $(".main__footer__formBtn").prop( 'disabled', false );
     });
   });
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
